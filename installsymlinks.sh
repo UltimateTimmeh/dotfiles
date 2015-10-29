39ae2b9c450a9b6cc88b053ae7b6a65bbb8facd8
UltimateTimmeh <dezutter.tim@gmail.com>
2015-10-29 09:50:44 +0100

    Added backing up of existing configurations to installation file
    A backup directory is created in $DOTROOT, with incrementing subdirectories,
    to which existing configuration files are moved if they exist.

--- installsymlinks.sh
+++ installsymlinks.sh
@@ -10,25 +10,35 @@ else
     MODE=$1
 fi
 
+# Create a backup directory, don't overwrite existing ones.
+NR=0
+while [ -d $DOTROOT/backups/$NR ]; do
+    NR=`expr $NR + 1`
+done
+BAKDIR=$DOTROOT/backups/$NR
+mkdir -pv $BAKDIR
+
 # Find all dotfiles and create symlinks to them in the home directory.
 DOTFILES=$(find $DOTROOT -maxdepth 1 -type f \( -iname '.*' ! -iname '*ignore' ! -iname '*~' \))
 for DOTFILE in $DOTFILES; do
     BASE=$(basename $DOTFILE)
     TGT=$HOME/$BASE
-    printf "Linking dotfile $BASE to $TGT... "
+    if [ -f $TGT ]; then
+	printf "Backing up existing $BASE... "
+	mv $TGT $BAKDIR
+	printf "DONE\n"
+    fi
+    printf "Linking $DOTFILE to $TGT... "
     ln -s $MODE $DOTFILE $TGT
     printf "DONE\n"
 done
 
-# Link all binaries in bin to $HOME/bin
-mkdir -pv $HOME/bin
-BINARIES=$(find $DOTROOT/bin -type f \( ! -iname '*~' \))
-for BINARY in $BINARIES; do
-    BASE=$(basename $BINARY)
-    TGT=$HOME/bin/$BASE
-    printf "Linking binary $BASE to $TGT... "
-    ln -s $MODE $BINARY $TGT
+# Link $HOME/bin to dotfiles bin directory.
+if [ -d $HOME/bin ]; then
+    printf "Backing up existing $HOME/bin... "
+    mv $HOME/bin $BAKDIR
     printf "DONE\n"
-done
-
-
+fi
+printf "Linking directory of binaries $DOTROOT/bin to $HOME/bin... "
+ln -sn $MODE $DOTROOT/bin $HOME/bin
+printf "DONE\n"
