f847de466b1d2732be066a24659fde0b98c10173
UltimateTimmeh <dezutter.tim@gmail.com>
2015-10-29 10:30:52 +0100

    Created a generalized function for backing up and symlinking

--- installsymlinks.sh
+++ installsymlinks.sh
@@ -3,13 +3,6 @@
 # Get path to the root of the dotfiles repository
 DOTROOT=$(dirname $(pwd)/$0)
 
-# Set mode (default -f, could later be based on inline arguments).
-if [ $# = 0 ]; then
-    MODE=-f
-else
-    MODE=$1
-fi
-
 # Create a backup directory, don't overwrite existing ones.
 NR=0
 while [ -d $DOTROOT/backups/$NR ]; do
@@ -18,27 +11,29 @@ done
 BAKDIR=$DOTROOT/backups/$NR
 mkdir -pv $BAKDIR
 
-# Find all dotfiles and create symlinks to them in the home directory.
-DOTFILES=$(find $DOTROOT -maxdepth 1 -type f \( -iname '.*' ! -iname '*ignore' ! -iname '*~' \))
-for DOTFILE in $DOTFILES; do
-    BASE=$(basename $DOTFILE)
-    TGT=$HOME/$BASE
-    if [ -f $TGT ]; then
-	printf "Backing up existing $BASE... "
-	mv $TGT $BAKDIR
+# General function for backing up and symlinking configurations.
+# $1 = Symbolic link target.
+# $2 = Symbolic link path.
+# $3 = Path of directory to which existing symlinkpaths are backed up.
+backup_and_link() {
+    if [ -e $2 ]; then
+	printf "Backup up existing $2... "
+	mv $2 $3
 	printf "DONE\n"
     fi
-    printf "Linking $DOTFILE to $TGT... "
-    ln -s $MODE $DOTFILE $TGT
+    printf "Linking $2 to $1... "
+    ln -sn $1 $2
     printf "DONE\n"
+}
+
+# Create symlinks to dotfiles.
+DOTFILES=$(find $DOTROOT -maxdepth 1 -type f \( -iname '.*' ! -iname '*ignore' ! -iname '*~' \))
+for DOTFILE in $DOTFILES; do
+    backup_and_link $DOTFILE $HOME/$(basename $DOTFILE) $BAKDIR
 done
 
-# Link $HOME/bin to dotfiles bin directory.
-if [ -d $HOME/bin ]; then
-    printf "Backing up existing $HOME/bin... "
-    mv $HOME/bin $BAKDIR
-    printf "DONE\n"
-fi
-printf "Linking directory of binaries $DOTROOT/bin to $HOME/bin... "
-ln -sn $MODE $DOTROOT/bin $HOME/bin
-printf "DONE\n"
+# Create symlink to binary directory.
+backup_and_link $DOTROOT/bin $HOME/bin $BAKDIR
+
+# Create symlinks to emacs config directory.
+backup_and_link $DOTROOT/.emacs.d $HOME/.emacs.d $BAKDIR
