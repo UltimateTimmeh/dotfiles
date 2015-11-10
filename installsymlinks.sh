2a23812913ca4063a3181db98d41a57b749fe030
UltimateTimmeh <dezutter.tim@gmail.com>
2015-11-10 08:39:09 +0100

    Added ssh config and updated configuration of backup paths in symlink script

--- installsymlinks.sh
+++ installsymlinks.sh
@@ -3,13 +3,12 @@
 # Get path to the root of the dotfiles repository
 DOTROOT=$(dirname $(pwd)/$0)
 
-# Create a backup directory, don't overwrite existing ones.
+# Configure a backup directory, don't overwrite existing ones.
 NR=0
 while [ -d $DOTROOT/backups/$NR ]; do
     NR=`expr $NR + 1`
 done
 BAKDIR=$DOTROOT/backups/$NR
-mkdir -pv $BAKDIR
 
 # General function for backing up and symlinking configurations.
 # $1 = Symbolic link target.
@@ -17,9 +16,10 @@ mkdir -pv $BAKDIR
 # $3 = Path of directory to which existing symlinkpaths are backed up.
 backup_and_link() {
     if [ -e $2 ]; then
-	printf "Back up existing $2... "
+        echo "Back up existing $2... "
+	mkdir -pv $3
 	mv $2 $3
-	printf "DONE\n"
+	echo "DONE"
     fi
     printf "Linking $2 to $1... "
     ln -sn $1 $2
@@ -35,13 +35,17 @@ done
 # Create symlink to binary directory.
 backup_and_link $DOTROOT/bin $HOME/bin $BAKDIR
 
+# Create symlink to ssh config.
+mkdir -pv $HOME/.ssh
+backup_and_link $DOTROOT/.ssh/config $HOME/.ssh/config $BAKDIR/.ssh
+
 # Create symlink to xfce4-terminal config directory.
 mkdir -pv $HOME/.config/xfce4
-backup_and_link $DOTROOT/terminal $HOME/.config/xfce4/terminal $BAKDIR
+backup_and_link $DOTROOT/terminal $HOME/.config/xfce4/terminal $BAKDIR/.config/xfce4
 
 # Create symlink to emacs config directory.
 backup_and_link $DOTROOT/.emacs.d $HOME/.emacs.d $BAKDIR
 
 # Create symlink to pyFormex config directory.
 mkdir -pv $HOME/.config
-backup_and_link $DOTROOT/pyformex $HOME/.config/pyformex $BAKDIR
+backup_and_link $DOTROOT/pyformex $HOME/.config/pyformex $BAKDIR/.config
